d58974fbbdbae55f5eb510f7861c41b5fba6099c
Marika <ferrari.marika@gmail.com>
2023-10-26 14:39:24 +0100

    CORS

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 import express from "express";
 import morgan from "morgan";
+
+//Importing cors to authorize connection of the two repos
+import cors from "cors";
+
 import { projectsRoutes } from "./Routes/projectsRoutes.js";
 
 const app = express();
@@ -7,6 +11,14 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.static("public"));
+
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5501",
+    methods: ["GET", "POST", "DELETE", "PATCH"],
+  })
+);
+
 app.use("/projects", projectsRoutes);
 
 export default app;
